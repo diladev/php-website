be8d1708ec6cc0cfc8e5390499fb09b28893d358
diladev <97544088+diladev@users.noreply.github.com>
2022-01-11 20:24:10 +0300

    Update dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,7 +1,7 @@
 <?php
 $server_name = "sql11.freemysqlhosting.net";
 $db_username = "sql11464987";
-$db_password = " X8rxn1uK4R";
+$db_password = "X8rxn1uK4R";
 $db_name = "sql11464987";
 
 $conn = mysqli_connect($server_name, $db_username, $db_password, $db_name);
