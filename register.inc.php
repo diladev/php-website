fc62e83176a96cfdd85496f2c6d0252b23a1cf26
diladev <97544088+diladev@users.noreply.github.com>
2022-01-14 00:00:19 +0300

    Update register.inc.php

--- register.inc.php
+++ register.inc.php
@@ -1,21 +1,27 @@
 <?php
 session_start();
 include 'dbh.inc.php';
-$studentName=$_SESSION["usersid"];
-$courseName = $_POST["courseName"];
-$creditHour = $_POST["dateOfReg"];
 
-function addCourse($conn,$studentName,$courseName,$regDate){
+$studentId=$_SESSION["usersid"];
+$courseName = $_POST["courseName"];
+$regDate = $_POST["dateOfReg"];
+
+$course = mysqli_query($conn, "SELECT C_ID FROM courses WHERE course_name = '$courseName'");
+$cIdQ = mysqli_fetch_array($course);
+$cId = $cIdQ['C_ID'];
+
+function regCourse($conn,$studentId,$cId,$regDate){
   $sql = "INSERT INTO registration (s_id, c_id, Registration_Date) VALUES (?, ?, ?);";
   $stmt = mysqli_stmt_init($conn);
   if (!mysqli_stmt_prepare($stmt, $sql)) {
     header("location: ./registerCourse.php?error=stmtfailed");
     exit();
 }
-mysqli_stmt_bind_param($stmt, "sss",$studentName,$courseName,$regDate);
+mysqli_stmt_bind_param($stmt, "sss",$studentId,$cId,$regDate);
 mysqli_stmt_execute($stmt);
 mysqli_stmt_close($stmt);
-header("location: ./registerCourse.php?error=none");
+header("location: ./registerCourse.inc.php?error=none");
 exit();
 }
-addCourse($conn,$courseName,$creditHour);
+
+regCourse($conn,$studentId,$cId,$regDate);
