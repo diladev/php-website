3c12d3e98f554ea8909bed58559a0a9d3ababa82
diladev <97544088+diladev@users.noreply.github.com>
2022-01-16 23:46:17 +0300

    Update newAbsent.php

--- newAbsent.php
+++ newAbsent.php
@@ -12,57 +12,18 @@ $result = mysqli_query($conn,'SELECT * FROM attendence');
     <link rel="stylesheet" href="NavBarStyle.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <style media="screen">
-    table {
-      border: 1px solid #CCC;
-      border-collapse: collapse;
-    }
+      table {
+        border: 1px solid #CCC;
+        border-collapse: collapse;
+      }
 
-    td {
-      border: 1px solid #CCC;
-    }
+      td {
+        border: 1px solid #CCC;
+      }
     </style>
   </head>
   <body>
-    <div class="flex-NavBar">
-      <ul class="nav-links">
-        <li>
-          <a href="newAddcourse.php" class="fancy-text">
-            <i class='bx bxs-book-add' ></i>
-            <span class="link_name"></span>
-          </a>
-        </li>
-        <li>
-          <a href="newAbsent.php" class="fancy-text">
-            <i class='bx bx-task'></i>
-            <span class="link_name"></span>
-          </a>
-        </li>
-        <li>
-          <a href="newAdduser.php" class="fancy-text">
-            <i class='bx bxs-user-plus'></i>
-            <span class="link_name"></span>
-          </a>
-        </li>
-        <li>
-          <a href="newTake.php" class="fancy-text">
-            <i class='bx bxs-user-check' ></i>
-            <span class="link_name"></span>
-          </a>
-        </li>
-        <li>
-          <a href="adminHomePage.php" class="fancy-text">
-            <i class='bx bx-home'></i>
-            <span class="link_name"></span>
-          </a>
-        </li>
-          <li>
-            <a href="logout.inc.php" class="fancy-text">
-              <i class='bx bx-log-out' ></i>
-              <span class="link_name"></span>
-            </a>
-          </li>
-  </ul>
-    </div>
+    <?php include_once 'AdminSideBar.php' ?>
     <section class="home-section">
       <div class="home-content">
         <table align="center" border="1" style="width: 600px; line-height: 50px;">
