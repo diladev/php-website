6c8d020ecdf0220d5040a8d5cc752f6518807fec
diladev <97544088+diladev@users.noreply.github.com>
2022-01-16 23:44:38 +0300

    Update adminHomePage.php

--- adminHomePage.php
+++ adminHomePage.php
@@ -12,91 +12,52 @@ $result = mysqli_query($conn,'SELECT * FROM attendence');
     <link rel="stylesheet" href="NavBarStyle.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <style media="screen">
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
 
-body{
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-}
+      body{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+      }
 
-.dynamic-txts{
-  margin-left: 15px;
-  height: 90px;
-  line-height: 90px;
-  overflow: hidden;
-  color: white;
-  font-size: 40px;
-}
-.dynamic-txts h2{
-  position: relative;
-  margin: 5px 0;
-  line-height: 90px;
-}
-.dynamic-txts h2::after{
-  content: "";
-  position: absolute;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  background: #BEDCE9;
-  border-left: 2px solid #4F7F96;
-  animation: typing 3s steps(10) infinite;
-}
-@keyframes typing {
-  40%, 60%{
-    left: calc(100% + 30px);
-  }
-  100%{
-    left: 0;
-  }
-}
+      .dynamic-txts{
+        margin-left: 15px;
+        height: 90px;
+        line-height: 90px;
+        overflow: hidden;
+        color: white;
+        font-size: 40px;
+      }
+      .dynamic-txts h2{
+        position: relative;
+        margin: 5px 0;
+        line-height: 90px;
+      }
+      .dynamic-txts h2::after{
+        content: "";
+        position: absolute;
+        left: 0;
+        height: 100%;
+        width: 100%;
+        background: #BEDCE9;
+        border-left: 2px solid #4F7F96;
+        animation: typing 3s steps(10) infinite;
+      }
+      @keyframes typing {
+        40%, 60%{
+          left: calc(100% + 30px);
+        }
+        100%{
+          left: 0;
+        }
+      }
     </style>
   </head>
   <body>
-    <div class="flex-NavBar">
-      <ul class="nav-links">
-        <li>
-          <a href="newAddcourse.php" class="fancy-text">
-            <i class='bx bxs-book-add' ></i>
-            <span class="link_name"></span>
-          </a>
-        </li>
-        <li>
-          <a href="newAbsent.php" class="fancy-text">
-            <i class='bx bx-task'></i>
-            <span class="link_name"></span>
-          </a>
-        </li>
-        <li>
-          <a href="newAdduser.php" class="fancy-text">
-            <i class='bx bxs-user-plus'></i>
-            <span class="link_name"></span>
-          </a>
-        </li>
-        <li>
-          <a href="newTake.php" class="fancy-text">
-            <i class='bx bxs-user-check' ></i>
-            <span class="link_name"></span>
-          </a>
-        </li>
-        <li>
-          <a href="adminHomePage.php" class="fancy-text">
-            <i class='bx bx-home'></i>
-            <span class="link_name"></span>
-          </a>
-        </li>
-          <li>
-            <a href="logout.inc.php" class="fancy-text">
-              <i class='bx bx-log-out' ></i>
-              <span class="link_name"></span>
-            </a>
-          </li>
-  </ul>
-    </div>
+    <?php include_once 'AdminSideBar.php' ?>
     <section class="dynamic-txts">
       <h2>Welcom To Admin Page</h2>
-      </section>
+    </section>
   </body>
 </html>
