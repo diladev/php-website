4e2f22dece06397b5e733e077d0908c675e7c1ff
diladev <97544088+diladev@users.noreply.github.com>
2022-01-16 23:42:05 +0300

    Add files via upload

---
+++ studentSideBar.php
@@ -0,0 +1,52 @@
+<div class="flex-NavBar">
+  <ul>
+    <div class="tooltip">
+      <li>
+        <a href="studentHome.php" class="navTexT" >
+          <i class='bx bx-home' ></i>
+          <span class="tooltiptext">Home</span>
+        </a>
+      </li>
+    </div>
+    <div class="tooltip">
+      <li>
+        <a href="newRecord.php" class="navTexT">
+          <i class='bx bx-task'></i>
+          <span class="tooltiptext ">Record</span>
+        </a>
+      </li>
+    </div>
+    <div class="tooltip">
+      <li>
+        <a href="#" class="navTexT">
+          <i class='bx bxs-dollar-circle'></i>
+          <span class="tooltiptext">Payment</span>
+        </a>
+      </li>
+    </div>
+    <div class="tooltip">
+      <li>
+        <a href="registerCourse.php" class="navTexT">
+          <i class='bx bxs-book-add'></i>
+          <span class="tooltiptext">Register Course</span>
+        </a>
+      </li>
+    </div>
+    <div class="tooltip">
+      <li>
+        <a href="registeredCourses.php" class="navTexT">
+          <i class='bx bxs-book-bookmark' ></i>
+          <span class="tooltiptext">Register Course</span>
+        </a>
+      </li>
+    </div>
+    <div class="tooltip">
+      <li>
+        <a href="logout.inc.php" class="navTexT">
+          <i class='bx bx-log-out' ></i>
+          <span class="tooltiptext">Logout</span>
+        </a>
+      </li>
+    </div>
+  </ul>
+</div>
