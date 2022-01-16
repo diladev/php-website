0104b065a51cd16ee99e669cfa60f8db4bf73455
diladev <97544088+diladev@users.noreply.github.com>
2022-01-17 01:15:09 +0300

    Update studentHome.php

--- studentHome.php
+++ studentHome.php
@@ -14,99 +14,48 @@ $result = mysqli_query($conn,'SELECT * FROM attendence');
     <style media="screen">
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
 
-body{
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-}
+    body{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      min-height: 100vh;
+    }
 
-.dynamic-txts{
-  margin-left: 15px;
-  height: 90px;
-  line-height: 90px;
-  overflow: hidden;
-  color: white;
-  font-size: 40px;
-}
-.dynamic-txts h2{
-  position: relative;
-  margin: 5px 0;
-  line-height: 90px;
-}
-.dynamic-txts h2::after{
-  content: "";
-  position: absolute;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  background: #BEDCE9;
-  border-left: 2px solid #4F7F96;
-  animation: typing 3s steps(10) infinite;
-}
-@keyframes typing {
-  40%, 60%{
-    left: calc(100% + 30px);
-  }
-  100%{
-    left: 0;
-  }
-}
+    .dynamic-txts{
+      margin-left: 15px;
+      height: 90px;
+      line-height: 90px;
+      overflow: hidden;
+      color: white;
+      font-size: 40px;
+    }
+    .dynamic-txts h2{
+      position: relative;
+      margin: 5px 0;
+      line-height: 90px;
+    }
+    .dynamic-txts h2::after{
+      content: "";
+      position: absolute;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      background: #BEDCE9;
+      border-left: 2px solid #4F7F96;
+      animation: typing 3s steps(10) infinite;
+    }
+    @keyframes typing {
+      40%, 60%{
+        left: calc(100% + 30px);
+      }
+      100%{
+        left: 0;
+      }
+    }
     </style>
   </head>
   <body>
-    <div class="flex-NavBar">
-     <ul>
-       <li>
-         <div class="tooltip">
-           <a href="studentHome.php" class="navTexT" >
-             <i class='bx bx-home'></i>
-             <span class="tooltiptext">Home</span>
-           </a>
-         </div>
-       </li>
-         <div class="tooltip">
-           <li>
-             <a href="newRecord.php" class="navTexT">
-               <i class='bx bx-task'></i>
-               <span class="tooltiptext ">Record</span>
-             </a>
-           </li>
-         </div>
-         <div class="tooltip">
-           <li>
-             <a href="#" class="navTexT">
-               <i class='bx bxs-dollar-circle'></i>
-               <span class="tooltiptext">Payment</span>
-             </a>
-           </li>
-         </div>
-         <div class="tooltip">
-           <li>
-             <a href="registerCourse.php" class="navTexT">
-               <i class='bx bxs-book-add'></i>
-               <span class="tooltiptext">Register Course</span>
-             </a>
-           </li>
-         </div>
-         <div class="tooltip">
-            <li>
-              <a href="registeredCourses.php" class="navTexT">
-                <i class='bx bxs-book-bookmark' ></i>
-                <span class="tooltiptext">Register Course</span>
-              </a>
-            </li>
-          </div>
-         <div class="tooltip">
-           <li>
-             <a href="logout.inc.php" class="navTexT">
-               <i class='bx bx-log-out' ></i>
-               <span class="tooltiptext">Logout</span>
-             </a>
-           </li>
-         </div>
-       </ul>
-    </div>
+    <?php include_once 'studentSideBar.php' ?>
     <section class="dynamic-txts">
       <h2>Welcom Dear Student</h2>
       </section>
