e8233478953b0c7ade3e5d6a4ddc6ba2864d37ef
diladev <97544088+diladev@users.noreply.github.com>
2022-01-17 01:23:23 +0300

    Update registeredCourses.php

--- registeredCourses.php
+++ registeredCourses.php
@@ -11,60 +11,7 @@ $result = mysqli_query($conn,"SELECT * FROM registration WHERE s_id = '".$_SESSI
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
   </head>
   <body>
-    <div class="flex-NavBar">
-      <ul>
-        <li>
-          <div class="tooltip">
-            <a href="studentHome.php" class="navTexT" >
-              <i class='bx bx-home' ></i>
-              <span class="tooltiptext">Home</span>
-            </a>
-          </div>
-        </li>
-          <div class="tooltip">
-            <li>
-              <a href="newRecord.php" class="navTexT">
-                <i class='bx bx-task'></i>
-                <span class="tooltiptext ">Record</span>
-              </a>
-            </li>
-          </div>
-          <div class="tooltip">
-            <li>
-              <a href="#" class="navTexT">
-                <i class='bx bxs-dollar-circle'></i>
-                <span class="tooltiptext">Payment</span>
-              </a>
-            </li>
-          </div>
-          <div class="tooltip">
-            <li>
-              <a href="registerCourse.php" class="navTexT">
-                <i class='bx bxs-book-add'></i>
-                <span class="tooltiptext">Register Course</span>
-              </a>
-            </li>
-          </div>
-          <div class="tooltip">
-            <li>
-              <a href="registeredCourses.php" class="navTexT">
-                <i class='bx bxs-book-bookmark' ></i>
-                <span class="tooltiptext">Register Course</span>
-              </a>
-            </li>
-          </div>
-          <div class="tooltip">
-            <li>
-              <a href="logout.inc.php" class="navTexT">
-                <i class='bx bx-log-out' ></i>
-                <span class="tooltiptext">Logout</span>
-              </a>
-            </li>
-          </div>
-
-  </ul>
-    </div>
-
+    <?php include_once 'studentSideBar.php' ?>
     <section class="home-section">
     <div class="home-content">
       <table align="center" border="1" style="margin-top: 0px;width: 600px; line-height: 50px;">
