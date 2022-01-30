35441d6db39959feddfb4a730db21aef19fb3ef8
diladev <97544088+diladev@users.noreply.github.com>
2022-01-30 22:30:35 +0300

    Update courses.php

--- courses.php
+++ courses.php
@@ -6,13 +6,43 @@ include 'dbh.inc.php';
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="NavBarStyle.css">
+   <link rel="stylesheet" href="HomPage.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
   </head>
   <body>
     <?php include_once 'studentSideBar.php' ?>
     <section class="home-section">
-    <div class="home-content">
+
+     
+     
+     <div class="container">
+      <h1 class="logo"></h1>
+
+      <nav>
+        <ul>
+      <ul class="nav-area">
+        <li><a class="fancy-text" href="index.php">Home</a></li>
+        <li><a class="fancy-text" href="courses.php">Courses</a></li>
+        <li><a class="fancy-text" href="About.php">About Us</a></li>
+        <li><a class="fancy-text" href="Contact.php">Contact Us</a></li>
+        <?php
+        if (isset($_SESSION["uRole"])  && $_SESSION["uRole"]=="admin"){
+            echo "<li><a class='fancy-text' href='adminHomePage.php'>Admin Page</a>";
+            echo "<li><a class='fancy-text' href='logout.inc.php'>Log out</a>";
+        }else if (isset($_SESSION["uRole"]) && $_SESSION["uRole"]=="student") {
+          echo "<li><a class='fancy-text' href='studentHome.php'>Profile Page</a>";
+          echo "<li><a class='fancy-text' href='logout.inc.php'>Log out</a>";
+        }else{
+          echo "<li><a class='fancy-text' href='mysite.php'>Login</a></li>";
+        }
+        ?>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="centerDiv" style="background:none; border: none;">
+
+          <div class="home-content">
       <table align="center" border="1" style="margin-top: 0px;width: 600px; line-height: 50px;">
         <tr>
           <th colspan="3" style="background-color: #474C55; color: white;"><h2>My Courses</h2></th>
@@ -39,6 +69,9 @@ include 'dbh.inc.php';
         ?>
       </table>
     </div>
+     
+     
+     
       </section>
   </body>
 </html>
