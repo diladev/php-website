a22a943c451f331e6d5ac40153ad57f41cf1b7db
diladev <97544088+diladev@users.noreply.github.com>
2022-01-30 22:53:03 +0300

    Update courses.php

--- courses.php
+++ courses.php
@@ -7,40 +7,25 @@ $course = mysqli_query($conn, "SELECT * FROM courses");
   <head>
     <meta charset="utf-8">
     <title></title>
-   <link rel="stylesheet" href="HomPage.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+       <style media="screen">
+    .centerDiv{
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #4F7F96;
+      border: solid 30px #4F7F96;
+      border-radius: 12px;
+      color: white;
+    }
+    </style>
   </head>
   <body>
     <section class="home-section">
 
-     
-     
-     <div class="container">
-      <h1 class="logo"></h1>
 
-      <nav>
-        <ul>
-      <ul class="nav-area">
-        <li><a class="fancy-text" href="index.php">Home</a></li>
-        <li><a class="fancy-text" href="courses.php">Courses</a></li>
-        <li><a class="fancy-text" href="About.php">About Us</a></li>
-        <li><a class="fancy-text" href="Contact.php">Contact Us</a></li>
-        <?php
-        if (isset($_SESSION["uRole"])  && $_SESSION["uRole"]=="admin"){
-            echo "<li><a class='fancy-text' href='adminHomePage.php'>Admin Page</a>";
-            echo "<li><a class='fancy-text' href='logout.inc.php'>Log out</a>";
-        }else if (isset($_SESSION["uRole"]) && $_SESSION["uRole"]=="student") {
-          echo "<li><a class='fancy-text' href='studentHome.php'>Profile Page</a>";
-          echo "<li><a class='fancy-text' href='logout.inc.php'>Log out</a>";
-        }else{
-          echo "<li><a class='fancy-text' href='mysite.php'>Login</a></li>";
-        }
-        ?>
-        </ul>
-      </nav>
-    </div>
-
-    <div>
+    <div class="centerDiv">
 
           <div class="home-content">
       <table align="center" border="1" style="margin-top: 0px;width: 600px; line-height: 50px;">
